6fdd7f8794bff05345a37d1ac96fa2ae87ee48d8
OTI-OWOM <ezeogouba@gmail.com>
2025-03-07 09:14:47 +0000

    fix: restart-nginx

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,13 +1,13 @@
-# increase nginx limit
+# Puppet manifest to increase NGINX ULIMIT to handle more connections
 exec { 'increase-limit':
-   command => 'sed -i "s/15/8192" /etc/default/nginx',
-   path    => '/usr/local/bin/:/bin/',
-   onlyif  => 'grep -q "15" /etc/default/nginx', 
+  command => 'sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/',
+  onlyif  => 'grep -q "ULIMIT=\"-n 15\"" /etc/default/nginx',
 }
 
-# restart nginx
-exec {
-   command => '/etc/init.d/nginx restart',
-   path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
-   require => Exec['increase-limit'],
+# restart nginx to apply new settings
+exec { 'restart-nginx':
+  command => '/etc/init.d/nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin',
+  require => Exec['increase-limit'],
 }
